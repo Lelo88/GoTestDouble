92d46a22b1926d2a9b64bcf3ad1ef8682540e7e0
Lelo88 <villalbaleandrogabriel@gmail.com>
2023-02-04 17:09:34 -0300

    exercise 3 partially realized

---
+++ simulator/simulator_test.go
@@ -0,0 +1,100 @@
+package simulator
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type simulatorMock struct{
+	CanCatchbool 	bool
+	GetLinearF 		float64
+	Error 			error
+}
+
+
+func (s *simulatorMock) CanCatch(distance float64, speed float64, catchSpeed float64) bool {
+	return s.CanCatchbool
+}
+
+func (s *simulatorMock) GetLinear(position [2]float64) float64 {
+	return s.GetLinearF
+}
+
+func (s *simulatorMock) ErrorIn() error {
+	return s.Error
+}
+
+func TestCanCatchAll(t *testing.T){
+	t.Run("prey catched true", func(t *testing.T){
+		ser := simulatorMock{
+			CanCatchbool: true,
+		}
+		s := catchSimulator{maxTimeToCatch: 10}
+		distance := 11
+		speed := 12
+		catchSpeed := 10
+		
+		timeToCatch := distance / (speed - catchSpeed)
+		timeToCatchBool := timeToCatch>0
+		canCatch := ser.CanCatch(float64(distance),float64(speed),float64(catchSpeed))
+		
+		assert.Equal(t, s.maxTimeToCatch>=float64(timeToCatch),canCatch)
+		assert.Equal(t, timeToCatchBool, canCatch)
+	})
+
+	t.Run("prey catched false", func(t *testing.T){
+		ser := simulatorMock{
+			CanCatchbool: true,
+		}
+		s := catchSimulator{maxTimeToCatch: 15}
+		distance := 20
+		speed := 1
+		catchSpeed :=0 
+		
+		timeToCatch := distance / (speed - catchSpeed)
+		maxTimeToCatch := s.maxTimeToCatch>=float64(timeToCatch)
+
+		timeToCatchBool := timeToCatch>0
+		canCatch := ser.CanCatch(float64(distance), float64(speed), float64(catchSpeed))
+		
+		assert.NotEqual(t, maxTimeToCatch, canCatch)
+		assert.Equal(t, timeToCatchBool, canCatch)
+	})
+
+	t.Run("error catched velocity", func(t *testing.T){
+		//s := catchSimulator{maxTimeToCatch: 15}
+		
+		ser := simulatorMock{
+			Error: errors.New("Division by zero"),
+		}
+		distance := 11
+		speed := 12
+		catchSpeed := 12
+
+		//timeToCatch := distance / (speed - catchSpeed)
+		//timeToCatchBool := timeToCatch>0
+		canCatch := ser.CanCatch(float64(distance), float64(speed), float64(catchSpeed))
+		dividend := speed - catchSpeed
+		
+
+		//assert.NotNil(t, timeToCatchBool, canCatch)
+		assert.Error(t, ser.Error)
+		assert.Error(t, ser.Error, canCatch)
+		assert.Equal(t, 0, dividend)
+		//assert.NotEqual(t,s.maxTimeToCatch>=float64(timeToCatch), canCatch)
+		//assert.NotEqual(t, s.maxTimeToCatch>=float64(timeToCatch), canCatch)
+	})
+}
+
+func TestGetLinearAll(t *testing.T){
+
+	t.Run("getting linear", func(t *testing.T){
+
+	})
+
+	t.Run("not getting linear", func(t *testing.T){
+
+	})
+}
