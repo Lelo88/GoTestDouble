3e77d26e4428efde9f3149547cf74fc10c16f585
Lelo88 <villalbaleandrogabriel@gmail.com>
2023-02-06 16:58:10 -0300

    mock prey instance ok and test ok catch whiteshark

---
+++ prey/mocks/prey_mock.go
@@ -0,0 +1,19 @@
+package mocks
+
+import "github.com/stretchr/testify/mock"
+
+//controller
+func NewTunaMock() *tunaMock{
+	return &tunaMock{}
+}
+
+//implementamos el mock de una estructura que ya esta inventada, en este caso, tuna
+type tunaMock struct {
+	mock.Mock
+} 
+
+//no recibe un argumento, pero que devuelva lo que va a pasar en el test
+func (t *tunaMock) GetSpeed() float64{
+	args := t.Called()
+	return args.Get(0).(float64)
+}
--- shark/white_shark_test.go
+++ shark/white_shark_test.go
@@ -3,20 +3,45 @@ package shark
 import (
 	"testing"
 
-	"github.com/Lelo88/GoTestDouble/simulator/mocks"
+	TunaMock "github.com/Lelo88/GoTestDouble/prey/mocks"
+	SimulatorMock "github.com/Lelo88/GoTestDouble/simulator/mocks"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
 func TestHunt(t *testing.T){
 	//arrante8
-	ms := mocks.NewCatchSimulatorMock()
+	ms := SimulatorMock.NewCatchSimulatorMock()
 	sh := CreateWhiteShark(ms) //acepta los metodos de la nueva interface
 
 
 
 	//act 
 	t.Run("ok", func(t *testing.T){
+		//arrange
+		
+		//ahora vamos a inicializar una presa mockeada y no harcodeada
+		prey := TunaMock.NewTunaMock()
+		prey.On("GetSpeed").Return(0.0)
+		ms.On("CanCatch", mock.Anything, mock.Anything, mock.Anything).Return(true)
+		//on le indica al mock como tiene que reaccionar ante ese resultado
+		
 
+		//hay que chequear el metodo getlinear
+		ms.On("GetLinearDistance", mock.Anything).Return(0.0)
+		//act
+		//aca llamamos al metodo para que el tiburon realice su caceria
+		//el metodo me pide un prey, por lo que voy a tener que realizar un mockeado, ya que lo implementa tuna, para ver que pasa al pasar valores mas reales
+		err := sh.Hunt(prey) 
+
+
+		//assert
+		assert.NoError(t, err)
+		ms.AssertExpectations(t)
+		prey.AssertExpectations(t)
 	})
+
+	//en el caso de que no haya llamado a todos los metodos (mas en ese orden) el test me iba a paniquear
 
 	t.Run("error", func(t *testing.T){
 
