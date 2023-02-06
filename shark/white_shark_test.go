b4b6e8601b227420686db2d996ba87a14a46c525
Lelo88 <villalbaleandrogabriel@gmail.com>
2023-02-06 10:24:08 -0300

    test hunted ok

---
+++ shark/white_shark_test.go
@@ -0,0 +1,47 @@
+package shark
+
+import (
+	"testing"
+
+	"github.com/Lelo88/GoTestDouble/prey"
+	"github.com/stretchr/testify/assert"
+)
+
+type mockWhiteShark struct{
+	Err error
+	WhiteShark whiteShark
+}
+
+func NewWhiteSharkService() *mockWhiteShark {
+	return &mockWhiteShark{}
+}
+
+func (m *mockWhiteShark) Hunt(prey prey.Prey ) error{
+	return m.Err
+}
+
+func TestHuntOrNot(t *testing.T){
+	
+	t.Run("hunted", func(t *testing.T){
+		ser := mockWhiteShark{
+			Err: nil,
+			WhiteShark: whiteShark{
+				speed: 12,
+				position: [2]float64{12,12},
+				simulator: NewWhiteSharkService().WhiteShark.simulator,
+			},
+		}
+
+		newTuna := prey.CreateTuna()
+
+		hunted :=ser.Hunt(newTuna)
+
+		assert.NoError(t, hunted)
+		assert.Equal(t, nil, hunted)
+
+	})
+
+	t.Run("not hunted", func(t *testing.T){
+
+	})
+}
